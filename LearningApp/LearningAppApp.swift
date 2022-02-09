4fa549e9efafc1ab38d2b43152e16c9f24ad6424
Iúri Tomé <iurifidalgo@gmail.com>
2022-02-09 10:54:08 +0000

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Iúri Sanchez Fidalgo Amaral Tomé on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
